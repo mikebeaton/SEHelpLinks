4f6be5371f1c6596da9f64f481c395c7574b2e80
MikeBeaton <mjsbeaton@gmail.com>
2020-03-06 14:26:42 +0000

    Back to .png

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 if [ -d "build" ]; then rm -rf build; fi
 mkdir build
-zip build/extension.zip add-links.js Help-Green-48x48.jpg Help-Green-128x128.jpg LICENSE manifest.json
+zip build/extension.zip add-links.js Help-Green-48x48.png Help-Green-128x128.png LICENSE manifest.json
