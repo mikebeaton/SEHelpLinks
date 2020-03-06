17dc7ccf83d4758b18599704c973fd138cff3914
MikeBeaton <mjsbeaton@gmail.com>
2020-03-06 00:04:40 +0000

    Chrome store seemed to accept .jpg and not .png version for 128x128 icon, so include this in manifest instead

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 if [ -d "build" ]; then rm -rf build; fi
 mkdir build
-zip build/extension.zip add-links.js Help-48x48.png Help-128x128.png LICENSE manifest.json
+zip build/extension.zip add-links.js Help-48x48.png Help-128x128.jpg LICENSE manifest.json
